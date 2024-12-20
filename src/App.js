27ef9d471044657a4b156056abd2ce42c894d8f7
Phuc <phucgianguyen0199@gmail.com>
2024-12-20 00:05:57 -0500

    Merge branch 'main' of https://github.com/PhucGiaNguyen99/react_file_uploader

---
+++ src/App.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import FileUpload form './components/FileUpload';
+import './App.css';
---
+++ src/components/FileUpload.js
@@ -0,0 +1,5 @@
+import React, { Fragment, useState } from 'react';
+import Message from './Message';
+import Progress from './Progress';
+import axios from 'axios';
+
